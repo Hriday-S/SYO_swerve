99e86a7aac12f1ab0e5baa431691d7e25c169963
unknown <syofrc@gmail.com>
2022-12-15 11:55:11 -0500

    Updated Deadband

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -114,11 +114,11 @@ public class RobotContainer {
     } else {
       return 0.0;
     }
-  }
+  } 
 
   private static double modifyAxis(double value) {
     // Deadband
-    value = deadband(value, 0.05);
+    value = deadband(value, 0.15);
 
     // Square the axis
     value = Math.copySign(value * value, value);
