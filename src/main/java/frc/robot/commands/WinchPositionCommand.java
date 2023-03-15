b9648a5c2a6ddd903f6c8214f730eda2d4ff5c0e
Darius Fan <fandar@syossetschools.org>
2023-03-15 13:03:44 -0400

    Updated Winch Intake Position

--- src/main/java/frc/robot/commands/WinchPositionCommand.java
+++ src/main/java/frc/robot/commands/WinchPositionCommand.java
@@ -17,7 +17,7 @@ public class WinchPositionCommand extends CommandBase {
         } else if (position.equals("OUT")) {
             m_targetAngle = 53;
         } else if (position.equals("IN")) {
-            m_targetAngle = 21;
+            m_targetAngle = 22;
         }
         m_power = power;
 
